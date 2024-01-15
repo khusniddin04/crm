e42b8dfb372df575319c31481199b662ba72ea1e
dior-05 <dioryuldoshev1@gmail.com>
2024-01-15 18:24:56 +0500

    dior5

--- src/pages/QabulPage.jsx
+++ src/pages/QabulPage.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import "../style/form.css"
-
 function QabulPage() {
-  
+  let full_name = useRef()
+  let phone = useRef()
+  let course = useRef()
+  let reception_time = useRef()
+  function handlClick (e) {
+    e.preventDefault()
+    let ready_to_push = {
+
+      full_name: full_name.current.value,
+      phone: phone.current.value,
+      course: course.current.value,
+      reception_time: reception_time.current.value, 
+    }
+    console.log(ready_to_push);
+      fetch("https://nurcrmapi.pythonanywhere.com/users/", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(ready_to_push)
+    })
+  }
   return (
     <div className='wrapper'>
     <form className='form' onSubmit={handlClick}>
@@ -10,28 +31,27 @@ function QabulPage() {
             <h3 className='title'>REGISTER</h3>                  
         </div>
         <label htmlFor="name">
-        <i class="fa-solid fa-user"></i>
+        <i className="fa-solid fa-user"></i>
             Name</label>
-        <input id='name' type="text" placeholder='F.I.O'required />
+        <input ref={full_name} id='name' type="text" placeholder='F.I.O'required />
 
         <label htmlFor="phone">
-        <i class="fa-solid fa-phone"></i>
+        <i className="fa-solid fa-phone"></i>
         Phone</label>
-        <input id='phone' type="number" placeholder='Telephone' required/>
+        <input ref={phone} id='phone' type="number" placeholder='Telephone' required/>
 
-        <label htmlFor="kurs"><i class="fa-solid fa-list"></i>Kurs</label>
-        <select name="kurs" id="" required>
+        <label htmlFor="kurs"><i className="fa-solid fa-list"></i>Kurs</label>
+        <select ref={course} name="kurs"  required>
             <optgroup label='Choose Kurs' >
-                <option value="" >Frontend</option>
-                <option value="" selected>Backend</option>
-                <option value="">Grafik dizayn</option>
-                <option value="">Web dizayn</option>
+                <option value="front" >Frontend</option>
+                <option value="back" >Backend</option>
+                <option value="gr">Grafik dizayn</option>
+                <option value="wg">Web dizayn</option>
             </optgroup>
         </select>
-
-        <label htmlFor="data"><i class="fa-solid fa-calendar-days"></i>Date</label>
-        <input id='data' type="date" required/> <br />
-        <button>Submit</button>
+        <label htmlFor="data"><i className="fa-solid fa-calendar-days"></i>Date</label>
+        <input ref={reception_time}  type="date" required/> <br />
+        <button type='submit'>Submit</button>
     </form>
 </div>
   )
